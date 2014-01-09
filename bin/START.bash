f48cff755846895cc038426417dc05b5cb5638eb
Bryan Lunt <lunt@ctbp.ucsd.edu>
2014-01-09 12:54:06 -0800

    Fixed START.bash script

--- bin/START.bash
+++ bin/START.bash
@@ -3,7 +3,8 @@
 # Absolute path to this script. /home/user/bin/foo.sh
 SCRIPT_ABS_PATH=$(readlink -f $0)
 # Absolute path to the directory this script is in. /home/user/bin
-SCRIPT_ABS_DIR=$(dirname $SCRIPT)
+SCRIPT_ABS_DIR=$(dirname $SCRIPT_ABS_PATH)
+BENCHMARK_SYS_DIR=$(dirname $SCRIPT_ABS_DIR)
 
-
+(cd ${BENCHMARK_SYS_DIR} ; ./bin/benchSubmit.py; )
 
